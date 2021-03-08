f705c3122e77ac59fceb1305174703b3495b4dd3
fikri azhari <fikriazhari551@gmail.com>
2021-03-08 13:16:11 +0700

    08 march 2021

---
+++ Pertemuan3/lat.py
@@ -0,0 +1,6 @@
+# tanpa break dan continue
+count = 0
+while (count < 9):
+    print(count)
+    count = count+1
+print('End of program')
---
+++ Pertemuan3/lat3.py
@@ -0,0 +1,8 @@
+# dengan continue
+count = 0
+while (count < 9):
+    count = count+1
+    if count == 4:
+        continue
+    print(count)
+print('End of program')
